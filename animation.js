5f4bc986bf28a95e991a0542247e69956c79415f
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-03-23 23:53:03 +0600

    Add max iteration counter

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 /**
  * csstime
- * Mon, 23 Mar 2015 17:37:49 GMT
+ * Mon, 23 Mar 2015 17:52:20 GMT
  */
-!function(){"use strict";function n(){snabbt(r,{position:e,delay:t,easing:"spring",springConstant:.3,springDeceleration:.8}).then({position:i,delay:o,easing:"easeIn"}),u()}function e(n){return[0,Number(r[n].getAttribute("data-y")),0]}function t(n){return n*c}function i(){return[0,0,0]}function o(n){return a+n*c}function u(){setTimeout(n,s)}if("addEventListener"in document){var r=document.getElementsByTagName("circle"),s=15e3,a=5e3,c=30;document.addEventListener("DOMContentLoaded",function(){setTimeout(n,1e3)})}}();
+!function(){"use strict";function n(){var n={position:t,delay:e,easing:"spring",springConstant:.3,springDeceleration:.8};return 0===d--?void snabbt(u,n):(snabbt(u,n).then({position:i,delay:o,easing:"easeIn"}),void r())}function t(n){return[0,Number(u[n].getAttribute("data-y")),0]}function e(n){return n*c}function i(){return[0,0,0]}function o(n){return s+n*c}function r(){setTimeout(n,a)}if("addEventListener"in document){var u=document.getElementsByTagName("circle"),a=15e3,s=5e3,c=30,d=10;document.addEventListener("DOMContentLoaded",function(){setTimeout(n,1e3)})}}();
