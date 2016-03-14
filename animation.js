3e1dcfe680e1cbfa360ef5bbf82f05ed92298ead
Julia Rechkunova <julia.rechkunova@gmail.com>
2016-03-14 23:04:06 +0100

    Update deps

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 /**
  * csstime
- * Thu, 23 Jul 2015 16:50:59 GMT
+ * Mon, 14 Mar 2016 21:57:43 GMT
  */
-!function(){"use strict";function n(){var n={position:t,delay:e,easing:"spring",springConstant:.3,springDeceleration:.8};return 0===d--?void snabbt(u,n):(snabbt(u,n).then({position:i,delay:o,easing:"easeIn"}),void r())}function t(n){return[0,Number(u[n].getAttribute("data-y")),0]}function e(n){return n*c}function i(){return[0,0,0]}function o(n){return s+n*c}function r(){setTimeout(n,a)}if("addEventListener"in document){var u=document.getElementsByTagName("circle"),a=15e3,s=5e3,c=30,d=10;document.addEventListener("DOMContentLoaded",function(){setTimeout(n,1e3)})}}();
+!function(){"use strict";function n(){var n={position:t,delay:e,easing:"spring",springConstant:.3,springDeceleration:.8};0!==d--&&(snabbt(u,n).snabbt({position:i,delay:o,easing:"easeIn"}),r())}function t(n){return[0,Number(u[n].getAttribute("data-y")),0]}function e(n){return n*c}function i(n){return[0,0,0]}function o(n){return s+n*c}function r(){setTimeout(n,a)}if("addEventListener"in document){var u=document.getElementsByTagName("circle"),a=15e3,s=3e3,c=30,d=1;document.addEventListener("DOMContentLoaded",function(){setTimeout(n,2e3)})}}();
