c5f93899bd41baa799c910b79646800739f7ede7
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-03-30 19:53:43 +0200

    Updates

--- animation.js
+++
@@ -1,5 +0,0 @@
-/**
- * csstime
- * Mon, 14 Mar 2016 21:57:43 GMT
- */
-!function(){"use strict";function n(){var n={position:t,delay:e,easing:"spring",springConstant:.3,springDeceleration:.8};0!==d--&&(snabbt(u,n).snabbt({position:i,delay:o,easing:"easeIn"}),r())}function t(n){return[0,Number(u[n].getAttribute("data-y")),0]}function e(n){return n*c}function i(n){return[0,0,0]}function o(n){return s+n*c}function r(){setTimeout(n,a)}if("addEventListener"in document){var u=document.getElementsByTagName("circle"),a=15e3,s=3e3,c=30,d=1;document.addEventListener("DOMContentLoaded",function(){setTimeout(n,2e3)})}}();
---
+++ precache-manifest.8f517e411910dae3c2a727819309119f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534249a71053cb4e2777b5ff47d5766f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d7b63b4b1ab2c6c54bf",
+    "url": "/static/css/main.2fc2b370.chunk.css"
+  },
+  {
+    "revision": "129e4e0fe7256254db62",
+    "url": "/static/js/2.6e095db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e095db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7b63b4b1ab2c6c54bf",
+    "url": "/static/js/main.e8e4b44d.chunk.js"
+  },
+  {
+    "revision": "fae39dd47ad9a00bb02c",
+    "url": "/static/js/runtime-main.c24f648c.js"
+  },
+  {
+    "revision": "2164c928f9f3c154430ef6024b2293a1",
+    "url": "/static/media/codepen.2164c928.svg"
+  },
+  {
+    "revision": "5ade927c1fe1f6525ce08cf7679e5974",
+    "url": "/static/media/feather-alt.5ade927c.svg"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "0729c42ac6c88ea12311c6989df785d6",
+    "url": "/static/media/twitter.0729c42a.svg"
+  }
+]);
